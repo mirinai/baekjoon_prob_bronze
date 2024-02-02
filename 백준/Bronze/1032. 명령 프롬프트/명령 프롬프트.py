a88c6c4751decb8b5fa89206a222a0f22bfcb07a
mirinai <154983447+mirinai@users.noreply.github.com>
2024-02-02 09:38:50 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -17,7 +17,6 @@ strng = ''
 
 for i in range(len(prom_list[0])):
     #문자열이 리스트로 있는 리스트에서 요소 하나하나 가져와서 집합을 만듦
-    #set은 겹치지 않으므로 만약 prom_list[0][i]==prom_list[1][i]==prom_list[2][i]라면 반드시 1이고 아니면 1보다 크다
     chars = set(prom_list[j][i] for j in range(N))
 
     if len(chars) == 1:
